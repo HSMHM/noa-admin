19aff97cd4883a2da3da0cdd44ad69ac8cfd7f34
Hassan Almalki <108739985+HSMHM@users.noreply.github.com>
2022-09-17 06:49:55 +0300

    ind finish

---
+++ ind/assets/js/chart/chartjs/chart.custom-home.js
@@ -0,0 +1,241 @@
+var ctx = document.getElementById("chart1");
+var myChart = new Chart(ctx, {
+    "type": "doughnut",
+    "data": {
+        "labels": [
+            "استعلام",
+            "رخص مهنية",
+            "انتساب"
+        ],
+        "datasets": [{
+            "label": "test",
+            "data": [
+                3,
+                2,
+                8
+            ],
+            "backgroundColor": [
+                "#085566",
+                "#00B6A1",
+                "#407FC1"
+            ]
+        }]
+    },
+    "options": {
+        "plugins": {
+            "legend": {
+                "display": false
+            },
+            "title": {
+                "display": true,
+                "text": 'تحت الدراسة',
+                "font": {
+                    "size": 16,
+                    "family": "'Noto Kufi Arabic', sans-serif"
+                },
+            }
+        },
+        "aspectRatio": 1
+    }
+});
+var ctx = document.getElementById("chart2");
+var myChart = new Chart(ctx, {
+    "type": "doughnut",
+    "data": {
+        "labels": [
+            "استعلام",
+            "رخص مهنية",
+            "انتساب"
+        ],
+        "datasets": [{
+            "label": "test",
+            "data": [
+                1,
+                7,
+                4
+            ],
+            "backgroundColor": [
+                "#085566",
+                "#00B6A1",
+                "#407FC1"
+            ]
+        }]
+    },
+    "options": {
+        "plugins": {
+            "legend": {
+                "display": false
+            },
+            "title": {
+                "display": true,
+                "text": 'غير مكتملة',
+                "font": {
+                    "size": 16,
+                    "family": "'Noto Kufi Arabic', sans-serif"
+                },
+            }
+        },
+        "aspectRatio": 1
+    }
+});
+var ctx = document.getElementById("chart3");
+var myChart = new Chart(ctx, {
+    "type": "doughnut",
+    "data": {
+        "labels": [
+            "استعلام",
+            "رخص مهنية",
+            "انتساب"
+        ],
+        "datasets": [{
+            "label": "test",
+            "data": [
+                4,
+                3,
+                5
+            ],
+            "backgroundColor": [
+                "#085566",
+                "#00B6A1",
+                "#407FC1"
+            ]
+        }]
+    },
+    "options": {
+        "plugins": {
+            "legend": {
+                "display": false
+            },
+            "title": {
+                "display": true,
+                "text": 'مرفوضة',
+                "font": {
+                    "size": 16,
+                    "family": "'Noto Kufi Arabic', sans-serif"
+                },
+            }
+        },
+        "aspectRatio": 1
+    }
+});
+var ctx = document.getElementById("chart4");
+var myChart = new Chart(ctx, {
+    "type": "doughnut",
+    "data": {
+        "labels": [
+            "استعلام",
+            "رخص مهنية",
+            "انتساب"
+        ],
+        "datasets": [{
+            "label": "test",
+            "data": [
+                7,
+                9,
+                7
+            ],
+            "backgroundColor": [
+                "#085566",
+                "#00B6A1",
+                "#407FC1"
+            ]
+        }]
+    },
+    "options": {
+        "plugins": {
+            "legend": {
+                "display": false
+            },
+            "title": {
+                "display": true,
+                "text": 'مشطوبة',
+                "font": {
+                    "size": 16,
+                    "family": "'Noto Kufi Arabic', sans-serif"
+                },
+            }
+        },
+        "aspectRatio": 1
+    }
+});
+var ctx = document.getElementById("chart5");
+var myChart = new Chart(ctx, {
+    "type": "doughnut",
+    "data": {
+        "labels": [
+            "استعلام",
+            "رخص مهنية",
+            "انتساب"
+        ],
+        "datasets": [{
+            "label": "test",
+            "data": [
+                6,
+                2,
+                9
+            ],
+            "backgroundColor": [
+                "#085566",
+                "#00B6A1",
+                "#407FC1"
+            ]
+        }]
+    },
+    "options": {
+        "plugins": {
+            "legend": {
+                "display": false
+            },
+            "title": {
+                "display": true,
+                "text": 'بإنتطار السداد',
+                "font": {
+                    "size": 16,
+                    "family": "'Noto Kufi Arabic', sans-serif"
+                },
+            }
+        },
+        "aspectRatio": 1
+    }
+});
+var ctx = document.getElementById("chart6");
+var myChart = new Chart(ctx, {
+    "type": "doughnut",
+    "data": {
+        "labels": [
+            "استعلام",
+            "رخص مهنية",
+            "انتساب"
+        ],
+        "datasets": [{
+            "label": "test",
+            "data": [
+                1,
+                7,
+                3
+            ],
+            "backgroundColor": [
+                "#085566",
+                "#00B6A1",
+                "#407FC1"
+            ]
+        }]
+    },
+    "options": {
+        "plugins": {
+            "legend": {
+                "display": false,
+            },
+            "title": {
+                "display": true,
+                "text": 'مقبولة',
+                "font": {
+                    "size": 16,
+                    "family": "'Noto Kufi Arabic', sans-serif"
+                },
+            }
+
+        },
+        "aspectRatio": 1
+    }
+});
